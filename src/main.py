5f4d7e3fc9c1ee170ef38da7d54b3734d9e592be
SurajJadhav7 <jadhavsuraj777@gmail.com>
2021-02-12 01:32:27 +0530

    Removed print msg

--- src/main.py
+++ src/main.py
@@ -9,14 +9,13 @@ worksheet.write('A1', 'Link Text')
 worksheet.write('B1', 'Link url')
 worksheet.write('C1', 'TC name')
 
-quote_page = 'https://github.com/'
+quote_page = 'https://google.com/'
 page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'lxml')
 
 for i, div in enumerate(soup.find_all('a')):
 	link_text = " ".join(str(div.text).split())
 	if link_text:
-		print(link_text)
 		worksheet.write('A'+str(i+2), link_text)
 		worksheet.write('B'+str(i+2), div.get('href'))
 		worksheet.write('C'+str(i+2), 'TC'+str(i+1)+'_'+link_text.lower()+'_click')
