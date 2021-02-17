2758e2c40b47b9fd120d441369166e944f886028
SurajJadhav7 <jadhavsuraj777@gmail.com>
2021-02-17 21:31:10 +0530

    added lxml parser in BeutifulSoup

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-# import xlsxwriter module 
 import xlsxwriter 
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
@@ -9,17 +8,21 @@ worksheet.write('A1', 'Link Text')
 worksheet.write('B1', 'Link url')
 worksheet.write('C1', 'TC name')
 
-quote_page = 'https://google.com/'
+quote_page = 'https://flipkart.com/'
 page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'lxml')
 
-for i, div in enumerate(soup.find_all('a')):
+for i, div in enumerate(soup.find_all('a', href=True)):
 	link_text = " ".join(str(div.text).split())
-	if link_text:
+	link_url = div.get('href')
+	if link_url is not None:
 		worksheet.write('A'+str(i+2), link_text)
-		worksheet.write('B'+str(i+2), div.get('href'))
+		if link_url.startswith("/"):
+			worksheet.write('B'+str(i+2), quote_page+link_url)
+		else:
+			worksheet.write('B'+str(i+2), link_url)
 		worksheet.write('C'+str(i+2), 'TC'+str(i+1)+'_'+link_text.lower()+'_click')
-	if i>15:
+	if i>100000:
 		break
 
 workbook.close() 
