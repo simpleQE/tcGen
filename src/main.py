26a9b1b3a19bc6678128e91d26db68083669467c
SurajJadhav7 <jadhavsuraj777@gmail.com>
2021-02-18 23:29:33 +0530

    Including anchor tags without href

--- src/main.py
+++ src/main.py
@@ -23,26 +23,26 @@ quote_page = 'https://flipkart.com/'
 home = urlparse(quote_page).netloc
 page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'lxml')
-anchors_list = soup.find_all('a', href=True)
+anchors_list = soup.find_all('a')
 for i, div in enumerate(anchors_list):
 	link_text = " ".join(str(div.text).split())
 	link_url = div.get('href')
-	if link_url is not None:
-		worksheet.write('A'+str(i+2), 'UC'+str(i+1)+'_'+link_text.lower()+'_click')
-		worksheet.write('B'+str(i+2), 'TC'+str(i+1)+'_'+link_text.lower()+'_click')
-		worksheet.write('C'+str(i+2), link_text)
-		worksheet.write('D'+str(i+2), 'Validating '+link_text+' link')
-		worksheet.write('E'+str(i+2), '['+home+']['+link_text+']')
-		worksheet.write('F'+str(i+2), 'Objective: To Validate opening of '+link_text+' link. \nPre-requisite - User should have desired access to the '+home+' . \nTest steps: \n1. Go to '+home+' .\n2. Click on '+link_text+' link.')
-		worksheet.write('G'+str(i+2), '1. '+link_text+' link should open.')
-		worksheet.write('H'+str(i+2), 'Functional')
+	if link_url is None:
+		if div.img:
+			link_url = div.img['src']
+	worksheet.write('A'+str(i+2), 'UC'+str(i+1)+'_'+link_text.lower()+'_click')
+	worksheet.write('B'+str(i+2), 'TC'+str(i+1)+'_'+link_text.lower()+'_click')
+	worksheet.write('C'+str(i+2), link_text)
+	worksheet.write('D'+str(i+2), 'Validating '+link_text+' link')
+	worksheet.write('E'+str(i+2), '['+home+']['+link_text+']')
+	worksheet.write('F'+str(i+2), 'Objective: To Validate opening of '+link_text+' link. \nPre-requisite - User should have desired access to the '+home+' . \nTest steps: \n1. Go to '+home+' .\n2. Click on '+link_text+' link.')
+	worksheet.write('G'+str(i+2), '1. '+link_text+' link should open.')
+	worksheet.write('H'+str(i+2), 'Functional')
+	if link_url.startswith("/"):
+		worksheet.write('K'+str(i+2), quote_page+link_url)
+	else:
+		worksheet.write('K'+str(i+2), link_url)
 	if i>100000:
 		break
 
-'''if link_url.startswith("/"):
-	worksheet.write('B'+str(i+2), quote_page+link_url)
-else:
-	worksheet.write('B'+str(i+2), link_url)'''
-# for j in range(i):
-# 	worksheet.set_column(i,i,30)
 workbook.close() 
